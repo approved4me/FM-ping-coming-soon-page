57ae1ef03b1ad173bdaf350d0960cbb49ece1dbc
approved4me <approved4me@gmail.com>
2023-01-12 17:43:26 +0100

    error message just for smartphne

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const email = document.getElementById("email");
 const form = document.getElementById("form");
-const errorElement = document.getElementById("error");
 const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 form.addEventListener("submit", (e) => {
     if (email.value === "" || email.value == null || !(email.value.match(pattern))) {
         e.preventDefault();
-        document.getElementsByClassName("email_invalid")[0].style.visibility = ("visible");
-        errorElement.innerText = ("Please provide a valid email");
-
+        document.getElementsByClassName("error")[0].style.display = ("block");
     }
     else {
         e.preventDefault();
         email.value = ("");
-        errorElement.innerText = ("Thanks for your subscription!");
-        document.getElementsByClassName("email_invalid")[0].style.visibility = ("hidden");
+        document.getElementsByClassName("error")[0].style.display = ("none");
     }
 
 });
