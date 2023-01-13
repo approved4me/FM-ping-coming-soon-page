6590d024375024215af22e6e881925967677a1b2
approved4me <approved4me@gmail.com>
2023-01-13 08:32:22 +0100

    final javascript touches

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ form.addEventListener("submit", (e) => {
     if (email.value === "" || email.value == null || !(email.value.match(pattern))) {
         e.preventDefault();
         document.getElementsByClassName("error")[0].style.display = ("block");
+        email.style.outline = ("2px solid var(--secondary--lightred)");
     }
     else {
         e.preventDefault();
         email.value = ("");
         document.getElementsByClassName("error")[0].style.display = ("none");
+        email.style.outline = ("1px solid var(--secondary-pale_blue)");
     }
 
 });
